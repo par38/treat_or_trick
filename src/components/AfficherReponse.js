f05944c7f9845ba0f5574e87b0421c31a94b190b
Monica TARABUSI <code.mta@gmail.com>
2018-10-30 15:25:13 +0100

    composants vides

---
+++ src/components/AfficherReponse.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class AfficherReponse extends Component {
+    state = {
+
+    }
+
+    render() {
+        return(
+            <div>
+
+            </div>
+        )
+
+    }
+}
+
+export default AfficherReponse;
