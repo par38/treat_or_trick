e15175b0d7ea856246c1a7545187011c48c141ae
Adam Ly <lyleelit@gmail.com>
2018-10-31 10:15:17 +0100

    resolve confict app.js

--- src/App.js
+++ src/App.js
@@ -3,15 +3,12 @@ import React, { Component } from 'react';
 import Questions from './components/Questions';
 import Spider from './components/Spider'
 import Background from './components/Background';
-<<<<<<< HEAD
 import ChampsSaisie from './components/ChampsSaisie';
 import Devinettes from './components/Devinettes';
+import BoutonReponse from './components/BoutonReponse'
 
 import './App.css';
 
-=======
-import BoutonReponse from './components/BoutonReponse'
->>>>>>> adding je donne ma langue
 
 
 class App extends Component {
